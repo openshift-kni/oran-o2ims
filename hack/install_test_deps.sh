0c1b21283d152ab811b1a10805b525c9f95790c4
Juan Hernandez <juan.hernandez@redhat.com>
2023-12-15 17:07:53 +0100

    MGMT-16112: Add OpenAPI handler
    This patch adds a new `/o2ims-infrastructureInventory/v1/openapi`
    endpoint that returns the OpenAPI specification of the service.
    
    Currently this specification only covers the metadata and the deployment
    manager endpoints. Other endpoints will be added later.
    
    Related: https://issues.redhat.com/browse/MGMT-16112
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- hack/install_test_deps.sh
+++ hack/install_test_deps.sh
@@ -14,3 +14,12 @@ if ! [ -x "$(command -v golangci-lint)" ]; then
     rm tarball
 fi
 
+
+if ! [ -x "$(command -v spectral)" ]; then
+    echo "Downloading spectral"
+
+    curl -Lo spectral https://github.com/stoplightio/spectral/releases/download/v6.11.0/spectral-linux-x64
+    echo 0e151d3dc5729750805428f79a152fa01dd4c203f1d9685ef19f4fd4696fcd5f spectral | sha256sum -c
+    chmod +x spectral
+    mv spectral $(go env GOPATH)/bin
+fi
