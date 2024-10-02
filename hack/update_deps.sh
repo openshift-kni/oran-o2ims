e93166d1a5029a404997bf6cec078b2c56fc7cfc
Don Penney <dpenney@redhat.com>
2024-10-02 13:54:46 -0400

    Update hack/update_deps.sh to handle exported API submodule
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- hack/update_deps.sh
+++ hack/update_deps.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+# Handle the exported api/hardwaremanagement submodule first
+pushd api/hardwaremanagement >/dev/null
+go mod tidy
+popd >/dev/null
+
 go mod vendor
 go mod tidy
 
