cd98bd0f6d267996c08719643b6cf23b4d6e7df3
Allain Legacy <alegacy@redhat.com>
2024-12-04 20:27:32 -0500

    CNF-15570: Add changes needed for upcoming collector
    This updates models and repository functions in preparation for the
    upcoming resource collector implementation.
    
    Signed-off-by: Allain Legacy <alegacy@redhat.com>

--- internal/service/resources/db/migrations/000001_baseline.up.sql
+++ internal/service/resources/db/migrations/000001_baseline.up.sql
@@ -1,11 +1,12 @@
 -- Table: data_source
 CREATE TABLE IF NOT EXISTS data_source
 (
-    id            SERIAL PRIMARY KEY,
+    data_source_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name          VARCHAR(255) NOT NULL,
     generation_id INTEGER      NOT NULL DEFAULT 0,                 -- incremented on each audit/sync
-    last_snapshot TIMESTAMPTZ           DEFAULT CURRENT_TIMESTAMP, -- last completed snapshot
-    created_at    TIMESTAMPTZ           DEFAULT CURRENT_TIMESTAMP  -- TBD; tracks when first imported
+    last_snapshot  TIMESTAMPTZ NULL,                               -- last completed snapshot
+    created_at     TIMESTAMPTZ      DEFAULT CURRENT_TIMESTAMP,     -- TBD; tracks when first imported
+    UNIQUE (name)
 );
 
 -- Table: resource_type
@@ -20,27 +21,27 @@ CREATE TABLE IF NOT EXISTS resource_type
     resource_kind    INTEGER      NOT NULL,                           -- enum of physical, logical, etc...
     resource_class   INTEGER      NOT NULL,                           -- enum of compute, networking, storage, etc...
     extensions       json         NULL,
-    data_source_id   INTEGER      NOT NULL,
+    data_source_id UUID NOT NULL,
     generation_id    INTEGER      NOT NULL DEFAULT 0,
     created_at       TIMESTAMPTZ           DEFAULT CURRENT_TIMESTAMP, -- TBD; tracks when first imported
-    FOREIGN KEY (data_source_id) REFERENCES data_source (id)          -- Manual cascade required for events
+    FOREIGN KEY (data_source_id) REFERENCES data_source (data_source_id) -- Manual cascade required for events
 );
 
 -- Table: resource_pool
 CREATE TABLE IF NOT EXISTS resource_pool
 (
     resource_pool_id   UUID PRIMARY KEY,
-    global_location_id UUID        NOT NULL,
+    global_location_id VARCHAR(64) NOT NULL,
     name               VARCHAR(255) NOT NULL,
     description        TEXT        NOT NULL,
     o_cloud_id         UUID         NOT NULL,
     location           VARCHAR(64) NULL,
     extensions         json         NULL,
-    data_source_id     INTEGER      NOT NULL,
+    data_source_id     UUID        NOT NULL,
     generation_id      INTEGER      NOT NULL DEFAULT 0,
     external_id VARCHAR(255) NOT NULL,                                  -- FQDN of resource in downstream data source (e.g., id=XXX)
     created_at         TIMESTAMPTZ           DEFAULT CURRENT_TIMESTAMP, -- TBD; tracks when first imported
-    FOREIGN KEY (data_source_id) REFERENCES data_source (id)            -- Manual cascade required for events
+    FOREIGN KEY (data_source_id) REFERENCES data_source (data_source_id) -- Manual cascade required for events
 );
 
 -- Table: resource
@@ -54,11 +55,11 @@ CREATE TABLE IF NOT EXISTS resource
     extensions       json         NULL,
     groups           VARCHAR(64)[] NULL,
     tags             VARCHAR(64)[] NULL,
-    data_source_id   INTEGER      NOT NULL,
+    data_source_id UUID NOT NULL,
     generation_id    INTEGER      NOT NULL DEFAULT 0,
     external_id      VARCHAR(255),                                    -- FQDN of resource in downstream data source (e.g., id=XXX)
     created_at       TIMESTAMPTZ           DEFAULT CURRENT_TIMESTAMP, -- TBD; tracks when first imported
-    FOREIGN KEY (data_source_id) REFERENCES data_source (id)          -- Manual cascade required for events
+    FOREIGN KEY (data_source_id) REFERENCES data_source (data_source_id) -- Manual cascade required for events
 );
 
 -- Table: resource_pool_member
@@ -74,7 +75,7 @@ CREATE TABLE IF NOT EXISTS resource_pool_member
 -- Table: deployment_manager
 CREATE TABLE IF NOT EXISTS deployment_manager
 (
-    cluster_id     UUID PRIMARY KEY,
+    deployment_manager_id UUID PRIMARY KEY,
     name           VARCHAR(255)  NOT NULL,
     description    TEXT          NOT NULL,
     o_cloud_id     UUID          NOT NULL,
@@ -83,20 +84,21 @@ CREATE TABLE IF NOT EXISTS deployment_manager
     capabilities   json          NULL,
     capacity_info  json          NULL,
     extensions     json          NULL,
-    data_source_id INTEGER       NOT NULL,
+    data_source_id        UUID NOT NULL,
     generation_id  INTEGER       NOT NULL DEFAULT 0,
     external_id    VARCHAR(255),                             -- FQDN of resource in downstream data source (e.g., id=XXX)
     created_at     TIMESTAMPTZ            DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (data_source_id) REFERENCES data_source (id) -- Manual cascade required for events
+    FOREIGN KEY (data_source_id) REFERENCES data_source (data_source_id) -- Manual cascade required for events
 );
 
 -- Table: event
 -- Description:  outbox pattern table
 CREATE TABLE IF NOT EXISTS data_change_event
 (
-    id           SERIAL PRIMARY KEY,
+    data_change_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     object_type  VARCHAR(64) NOT NULL, -- Table name reference
     object_id    UUID        NOT NUll, -- Primary key
+    parent_id      UUID NULL,
     before_state json        NULL,
     after_state json NULL,
     created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
@@ -105,7 +107,7 @@ CREATE TABLE IF NOT EXISTS data_change_event
 -- Table: subscription
 CREATE TABLE subscription
 (
-    subscription_id          UUID PRIMARY KEY,
+    subscription_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     consumer_subscription_id VARCHAR(64),
     filter                   TEXT,
     callback                 TEXT    NOT NULL,
