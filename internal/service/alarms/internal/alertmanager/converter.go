4a350656b7672476eacf3fadbc614e114c9b03e8
Nahian Pathan <npathan@redhat.com>
2025-03-04 09:43:14 -0500

    programmatically merge alertmanager.yaml instead of replace (#583)

--- internal/service/alarms/internal/alertmanager/converter.go
+++ internal/service/alarms/internal/alertmanager/converter.go
@@ -1,82 +1,15 @@
 package alertmanager
 
 import (
-	"bytes"
-	"context"
-	_ "embed"
-	"fmt"
 	"log/slog"
 	"maps"
-	"text/template"
 	"time"
 
 	"github.com/google/uuid"
-	corev1 "k8s.io/api/core/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	"github.com/openshift-kni/oran-o2ims/internal/controllers/utils"
 	api "github.com/openshift-kni/oran-o2ims/internal/service/alarms/api/generated"
 	"github.com/openshift-kni/oran-o2ims/internal/service/alarms/internal/db/models"
 	"github.com/openshift-kni/oran-o2ims/internal/service/alarms/internal/infrastructure"
-	"github.com/openshift-kni/oran-o2ims/internal/service/common/clients/k8s"
 )
-
-const (
-	namespace  = utils.AlertmanagerNamespace
-	secretName = "alertmanager-config"
-	secretKey  = "alertmanager.yaml"
-)
-
-const templateName = "alertmanager.yaml"
-
-//go:embed alertmanager.yaml.template
-var alertManagerConfig []byte
-
-var getHubClient = k8s.NewClientForHub
-
-// Setup updates the alertmanager config secret with the new configuration
-func Setup(ctx context.Context) error {
-	hubClient, err := getHubClient()
-	if err != nil {
-		return fmt.Errorf("error creating client for hub: %w", err)
-	}
-
-	// ACM recreates the secret when it is deleted, so we can safely assume it exists
-	var secret corev1.Secret
-	err = hubClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: secretName}, &secret)
-	if err != nil {
-		return fmt.Errorf("failed to get secret %s/%s: %w", namespace, secretName, err)
-	}
-
-	// Verify that Data has the key "alertmanager.yaml"
-	if _, ok := secret.Data[secretKey]; !ok {
-		return fmt.Errorf("%s not found in secret %s/%s", secretKey, namespace, secretName)
-	}
-
-	t, err := template.New(templateName).Parse(string(alertManagerConfig))
-	if err != nil {
-		return fmt.Errorf("failed to parse alertmanager.yaml: %w", err)
-	}
-
-	var rendered bytes.Buffer
-	err = t.ExecuteTemplate(&rendered, templateName, map[string]string{
-		"url":             utils.GetServiceURL(utils.InventoryAlarmServerName),
-		"caFile":          utils.DefaultServiceCAFile,
-		"bearerTokenFile": utils.DefaultBackendTokenFile,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to render alertmanager.yaml: %w", err)
-	}
-
-	secret.Data[secretKey] = rendered.Bytes()
-	err = hubClient.Update(ctx, &secret)
-	if err != nil {
-		return fmt.Errorf("failed to update secret %s/%s: %w", namespace, secretName, err)
-	}
-
-	slog.Info("Successfully configured alertmanager")
-	return nil
-}
 
 // ConvertAmToAlarmEventRecordModels get alarmEventRecords based on the alertmanager notification and AlarmDefinition
 func ConvertAmToAlarmEventRecordModels(am *api.AlertmanagerNotification, infrastructureClient infrastructure.Client) []models.AlarmEventRecord {
