363d295c7ad5784561c31c4098fa2b5e9adc12df
Raúl Hernández <rauherna@redhat.com>
2025-04-16 13:32:24 +0200

    add new version

--- .konflux/overlay.bash
+++ .konflux/overlay.bash
@@ -5,6 +5,8 @@ set -o nounset
 set -o pipefail
 
 #set -x
+# Debug mode off by default
+DEBUG=false
 
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCRIPT_NAME=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
@@ -13,58 +15,60 @@ MAP_STAGING="staging"
 MAP_PRODUCTION="production"
 MANAGER_KEY="manager_img"
 
+debug() {
+    if [ "$DEBUG" = true ]; then
+        echo "[DEBUG] $1"
+    fi
+}
+
 check_preconditions() {
     echo "Checking pre-conditions..."
 
     # yq must be installed
-    command -v yq >/dev/null 2>&1 || { echo "Error: yq seems not to be installed.Exit!" >&2; exit 1; }
+    command -v yq >/dev/null 2>&1 || { echo "Error: yq seems not to be installed." >&2; exit 1; }
     echo "Checking pre-conditions completed!"
     return 0
 }
 
-overlay_images() {
-    echo "Overlaying images (sha256)..."
+pin_images() {
+    echo "Pinning images (sha256)..."
 
     for image_name in "${!IMAGE_TO_SOURCE[@]}"; do
-        echo "replacing: image_name: $image_name, source: ${IMAGE_TO_SOURCE[$image_name]}, target: ${IMAGE_TO_TARGET[$image_name]}"
+        echo "Replacing: image_name: $image_name, source: ${IMAGE_TO_SOURCE[$image_name]}, target: ${IMAGE_TO_TARGET[$image_name]}"
         sed -i "s,${IMAGE_TO_SOURCE[$image_name]},${IMAGE_TO_TARGET[$image_name]},g" $ARG_CSV_FILE
     done
 
-    echo "Overlaying images completed!"
+    echo "Pinning images completed!"
     return 0
 }
 
-overlay_related_images() {
-    echo "Overlaying related images..."
+add_related_images() {
+    echo "Adding related images..."
 
     # remove the existing section
+    echo "Removing .spec.relatedImages"
     yq e -i 'del(.spec.relatedImages)' $ARG_CSV_FILE
 
     # create a new section from scratch
     declare -i index=0
     for image_name in "${!IMAGE_TO_SOURCE[@]}"; do
-        echo "adding related image: image_name: $image_name source: ${IMAGE_TO_SOURCE[$image_name]}, target: ${IMAGE_TO_TARGET[$image_name]}"
+        echo "Adding related image: image_name: $image_name source: ${IMAGE_TO_SOURCE[$image_name]}, target: ${IMAGE_TO_TARGET[$image_name]}"
         yq e -i ".spec.relatedImages[$index].name=\"$image_name\" |
                  .spec.relatedImages[$index].value=\"${IMAGE_TO_TARGET[$image_name]}\"" $ARG_CSV_FILE
         index=$index+1
     done
 
-    echo "Overlaying related images completed!"
+    echo "Adding related images completed!"
     return 0
 }
 
-parse_map_images_file() {
-    echo "Parsing map image file..."
-
-    if [[ ! -f "$ARG_MAP_FILE" ]]; then
-        echo "Error: File '$ARG_MAP_FILE' not found. Exit!" >&2
-        exit 1
-    fi
+parse_mapping_images_file() {
+    echo "Parsing mapping image file..."
 
     # Extract keys and images
-    local keys=($(yq eval '.[].key' "$ARG_MAP_FILE"))
-    local staging_images=($(yq eval '.[].staging' "$ARG_MAP_FILE"))
-    local production_images=($(yq eval '.[].production' "$ARG_MAP_FILE"))
+    local keys=($(yq eval '.[].key' "$ARG_MAPPING_FILE"))
+    local staging_images=($(yq eval '.[].staging' "$ARG_MAPPING_FILE"))
+    local production_images=($(yq eval '.[].production' "$ARG_MAPPING_FILE"))
     local entries=${#keys[@]}
 
     # Declare associative arrays
@@ -79,17 +83,25 @@ parse_map_images_file() {
         IMAGE_TO_PRODUCTION["$key"]="${production_images[i]}"
     done
 
-    echo "Parsing map image files completed!"
+    echo "Parsing mapping image file completed!"
     return 0
 }
 
 map_images() {
+
+    if [[ ! -f "$ARG_MAPPING_FILE" ]]; then
+        echo "Skipping images mapping!"
+        return 0
+    fi
+
     echo "Mapping images ..."
 
-    parse_map_images_file
+    parse_mapping_images_file
 
     for image_name in "${!IMAGE_TO_TARGET[@]}"; do
         local image_name_target="${IMAGE_TO_TARGET[$image_name]}"
+
+        # requires an image already pinned, sha256 format: '...@sha256:..."
         local image_name_target_trimmed="${image_name_target%@*}"
 
         local image_name_target_trimmed_mapped=""
@@ -111,25 +123,25 @@ map_images() {
 
         fi
 
-        echo "replacing: image_name: $image_name, original: $image_name_target_trimmed, mapped: $image_name_target_trimmed_mapped"
+        echo "Replacing: image_name: $image_name, original: $image_name_target_trimmed, mapped: $image_name_target_trimmed_mapped"
         sed -i "s,$image_name_target_trimmed,$image_name_target_trimmed_mapped,g" $ARG_CSV_FILE
     done
 
     echo "Mapping images completed"
 }
 
-parse_overlay_images_file() {
-    echo "Parsing image files..."
+parse_pinning_images_file() {
+    echo "Parsing pinning file..."
 
-    if [[ ! -f "$ARG_IMAGES_FILE" ]]; then
-        echo "Error: File '$ARG_IMAGES_FILE' not found. Exit!" >&2
+    if [[ ! -f "$ARG_PINNING_FILE" ]]; then
+        echo "Error: File '$ARG_PINNING_FILE' not found. " >&2
         exit 1
     fi
 
     # Extract keys and images
-    local keys=($(yq eval '.[].key' "$ARG_IMAGES_FILE"))
-    local sources=($(yq eval '.[].source' "$ARG_IMAGES_FILE"))
-    local targets=($(yq eval '.[].target' "$ARG_IMAGES_FILE"))
+    local keys=($(yq eval '.[].key' "$ARG_PINNING_FILE"))
+    local sources=($(yq eval '.[].source' "$ARG_PINNING_FILE"))
+    local targets=($(yq eval '.[].target' "$ARG_PINNING_FILE"))
     local entries=${#keys[@]}
 
     # Declare associative arrays
@@ -144,7 +156,15 @@ parse_overlay_images_file() {
         IMAGE_TO_TARGET["$key"]="${targets[i]}"
     done
 
-    echo "Parsing image completed!"
+    if [ "$DEBUG" = true ]; then
+        for key in "${!IMAGE_TO_SOURCE[@]}"; do
+            echo "- key: $key"
+            echo "  source: ${IMAGE_TO_SOURCE[$name]}"
+            echo "  target: ${IMAGE_TO_TARGET[$name]}"
+        done
+    fi
+
+    echo "Parsing pinning file completed!"
     return 0
 }
 
@@ -153,15 +173,15 @@ parse_args() {
 
     # command line options
     local options=
-    local long_options="set-images-file:,set-map-file:,set-csv-file:,set-map-staging,set-map-production,help"
+    local long_options="set-pinning-file:,set-mapping-file:,set-csv-file:,set-mapping-staging,set-mapping-production,help"
 
     local parsed=$(getopt --options="$options" --longoptions="$long_options" --name "$SCRIPT_NAME" -- "$@")
     eval set -- "$parsed"
 
     local map_staging=0
     local map_production=0
-    declare -g ARG_MAP_FILE=""
-    declare -g ARG_IMAGES_FILE=""
+    declare -g ARG_MAPPING_FILE=""
+    declare -g ARG_PINNING_FILE=""
     declare -g ARG_CSV_FILE=""
     declare -g ARG_MAP=""
     while true; do
@@ -174,20 +194,20 @@ parse_args() {
                 ARG_CSV_FILE=$2
                 shift 2
                 ;;
-            --set-images-file)
-                ARG_IMAGES_FILE=$2
+            --set-pinning-file)
+                ARG_PINNING_FILE=$2
                 shift 2
                 ;;
-            --set-map-file)
-                ARG_MAP_FILE=$2
+            --set-mapping-file)
+                ARG_MAPPING_FILE=$2
                 shift 2
                 ;;
-            --set-map-staging)
+            --set-mapping-staging)
                 map_staging=1
                 ARG_MAP=$MAP_STAGING
                 shift 1
                 ;;
-            --set-map-production)
+            --set-mapping-production)
                 map_production=1
                 ARG_MAP=$MAP_PRODUCTION
                 shift 1
@@ -205,48 +225,48 @@ parse_args() {
     done
 
     # validate images file
-    if [[ -n $ARG_IMAGES_FILE && ! -f "$ARG_IMAGES_FILE" ]]; then
-        echo "Error: file '$ARG_IMAGES_FILE' does not exist.Exit!" >&2
+    if [[ -n $ARG_PINNING_FILE && ! -f "$ARG_PINNING_FILE" ]]; then
+        echo "Error: file '$ARG_PINNING_FILE' does not exist." >&2
         exit 1
     fi
 
     # validate csv file
     if [[ -n $ARG_CSV_FILE && ! -f "$ARG_CSV_FILE" ]]; then
-        echo "Error: file '$ARG_CSV_FILE' does not exist.Exit!" >&2
+        echo "Error: file '$ARG_CSV_FILE' does not exist." >&2
         exit 1
     fi
 
     # validate map options
     if [[ $map_staging -eq 1 && $map_production -eq 1 ]]; then
-        echo "Error: cannot specify both '--set-map-staging' and '--set-map-production'.Exit!" >&2
+        echo "Error: cannot specify both '--set-mapping-staging' and '--set-mapping-production'." >&2
         exit 1
     fi
 
     if [[ $map_staging -eq 1 || $map_production -eq 1 ]]; then
-        if [[ ! -n $ARG_MAP_FILE ]]; then
-            echo "Error: specify '--set-map-file' to use a container registry map file.Exit!" >&2
+        if [[ ! -n $ARG_MAPPING_FILE ]]; then
+            echo "Error: specify '--set-mapping-file' to use a container registry map file." >&2
+            exit 1
+        fi
+
+        if [[ ! -f "$ARG_MAPPING_FILE" ]]; then
+            echo "Error: file '$ARG_MAPPING_FILE' does not exist." >&2
             exit 1
         fi
     fi
 
-    if [[ $map_staging -eq 0 && $map_production -eq 0 ]]; then
-        if [[ -n $ARG_MAP_FILE ]]; then
-            echo "Error: specify '--set-map-staging' or '--set-map-production'.Exit!" >&2
+    if [[ -n $ARG_MAPPING_FILE ]]; then
+        if [[ $map_staging -eq 0 && $map_production -eq 0 ]]; then
+            echo "Error: specify '--set-mapping-staging' or '--set-mapping-production'." >&2
             exit 1
         fi
-    fi
-
-    if [[ -n $ARG_MAP_FILE && ! -f "$ARG_MAP_FILE" ]]; then
-        echo "Error: file '$ARG_MAP_FILE' does not exist.Exit!" >&2
-        exit 1
     fi
 
     echo "Parsing args completed!"
 }
 
-overlay_misc()
+overlay_release()
 {
-    echo "Overlaying misc..."
+    echo "Overlaying relase..."
 
     local display_name="o-cloud manager"
     local description="o-cloud-manager operator"
@@ -273,49 +293,59 @@ overlay_misc()
     # use this from 4.19.1 onwards
     # ./yq e -i ".spec.replaces = $replaces)" $ARG_CSV_FILE
 
-    echo "Overlaying misc completed!"
+    echo "Overlaying release completed!"
 }
 
 main() {
    check_preconditions
    parse_args "$@"
-   parse_overlay_images_file
-   overlay_images
-   overlay_related_images
-   overlay_misc
-   map_images    # this must always be the last action
-
-    # Access the arrays
-    echo "=== Image Mapping Summary ==="
-    for name in "${!IMAGE_TO_SOURCE[@]}"; do
-        echo "Name: $name"
-        echo "  Source: ${IMAGE_TO_SOURCE[$name]}"
-        echo "  Target: ${IMAGE_TO_TARGET[$name]}"
-        echo "------------------------------"
-    done
+   parse_pinning_images_file
+   pin_images
+   add_related_images
+   overlay_release
+   map_images    # this MUST always be the last action
 }
 
 usage() {
    cat << EOF
 NAME
 
-   $SCRIPT_NAME - overlay operator manifests for konflux
+   $SCRIPT_NAME - overlay operator csv
 
 SYNOPSIS
 
-   $SCRIPT_NAME --set-images-file FILE --set-csv-file FILE
+   $SCRIPT_NAME --set-pinning-file FILE [--set-mapping-file FILE (--set-mapping-staging|--set-mapping-production) --set-csv-file FILE
+
+EXAMPLES
+
+   - Pin (sha256) images on 'oran-o2ims.clusterserviceversion.yaml' according to the configuration on 'pin_images.in.yaml':
+
+     $ overlay.bash --set-pinning-file pin_images.in.yaml --set-csv-file oran-o2ims.clusterserviceversion.yaml
+
+   - Pin (sha256) images on 'oran-o2ims.clusterserviceversion.yaml' according to the configuration on 'pin_images.in.yaml'
+     and map them to the production registry according to the configuration on 'map_images.in.yaml':
+
+     $ overlay.bash --set-pinning-file pin_images.in.yaml --set-mapping-file map_images.in.yaml --set-mapping-production --set-csv-file oran-o2ims.clusterserviceversion.yaml
 
 DESCRIPTION
 
-   overlay operator manifests
+   overlay operator csv
 
 ARGS
 
-   --set-images-file FILE
-      Set the images file for the overlay
+   --set-pinning-file FILE
+      Set the pinning file to pin image refs to sha256
+
+   --set-mapping-file FILE
+      Set the mapping file to map image refs to another container registry
+
+      When used, it must be accompanied by either:
+
+        --set-mapping-staging    map to 'registry.stage.redhat.io'
+        --set-mapping-production map to 'registry.redhat.io'
 
    --set-csv-file FILE
-      Set the cluster service version file for the overlay
+      Set the cluster service version file
 
    --help
       Display this help and exit.
