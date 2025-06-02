1741ec44ba4f297e7a8e7575046f79f39f79bce0
Sharat Akhoury <sharat.saurabh@gmail.com>
2025-06-02 12:00:15 -0400

    Add client generated code for the plugin API (#707)
    Signed-off-by: Sharat Akhoury <sakhoury@redhat.com>

--- hwmgr-plugins/api/tools/generate.go
+++ hwmgr-plugins/api/tools/generate.go
@@ -6,4 +6,6 @@ SPDX-License-Identifier: Apache-2.0
 
 package tools
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen.yaml ../openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen-server.yaml ../openapi.yaml
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen-client.yaml ../openapi.yaml
