44699b4bd8d200165fdca3abcea9bd2ce9076dde
Don Penney <dpenney@redhat.com>
2025-09-30 11:45:45 -0400

    Update markdownlint-cli2 version and resolve warnings
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- hack/install-markdownlint.sh
+++ hack/install-markdownlint.sh
@@ -9,11 +9,10 @@
 cat /etc/redhat-release || echo "No /etc/redhat-release"
 
 if grep -q 'Red Hat Enterprise Linux' /etc/redhat-release; then
-    # install the config file for the RPM repository with node 14
-    # steps taken from https://rpm.nodesource.com/setup_14.x
+    # https://github.com/nodesource/distributions/blob/master/DEV_README.md#enterprise-linux-based-distributions
     yum module disable -y nodejs
-    curl -sL -o '/tmp/nodesource.rpm' 'https://rpm.nodesource.com/pub_14.x/el/8/x86_64/nodesource-release-el8-1.noarch.rpm'
-    rpm -i --nosignature --force /tmp/nodesource.rpm
+    curl -fsSL https://rpm.nodesource.com/setup_23.x -o nodesource_setup.sh
+    bash nodesource_setup.sh
     yum -y install nodejs
 else
     # Fedora has a module we can use
@@ -21,4 +20,4 @@ else
     dnf -y install nodejs
 fi
 
-npm install -g markdownlint@v0.25.1 markdownlint-cli2@v0.4.0
+npm install -g markdownlint@v0.38.0 markdownlint-cli2@v0.18.1 --save-dev
